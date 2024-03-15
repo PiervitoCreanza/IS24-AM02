d4983b91917cdf176b3d16e5a0f520e0c0a83d22
marco <marcofebbo02@hotmail.it>
2024-03-15 17:20:24 +0100

    Add the enum and abstract Class Card

---
+++ src/main/java/it/polimi/ingsw/model/Card.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.model;
+/**
+ * Abstract class representing a card in the game.
+ * This class serves as a generic abstraction for all types of cards present in the game.
+ */
+abstract public class Card {
+
+}
