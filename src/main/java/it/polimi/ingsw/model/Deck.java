c4d6f7cde47e70f249a7f912e49e678214865867
simonecurci <simonefrancesco.curci@mail.polimi.it>
2024-03-18 20:49:01 +0100

    Added Deck class

---
+++ src/main/java/it/polimi/ingsw/model/Deck.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw.model;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * This class represents a deck of cards in the game.
+ * It contains a list of cards and provides a method to draw a card from the deck.
+ * @param <T> The type of cards in the deck (GameCard or ObjectiveCard)
+ */
+public class Deck<T> {
+    private final ArrayList<T> deck;
+    private final Random random;
+
+    /**
+     * Constructor for Deck. Initializes the deck with the specified list of cards.
+     * @param cards The list of cards to initialize the deck with.
+     */
+    public Deck(ArrayList<T> cards) {
+        this.deck = new ArrayList<T>(cards);
+        this.random = new Random();
+    }
+
+    /**
+     * Draws a random card from the deck. The card is removed from the deck.
+     * @return The card drawn from the deck.
+     */
+    public T draw(){
+        return deck.remove(random.nextInt(deck.size()));
+    }
+}
