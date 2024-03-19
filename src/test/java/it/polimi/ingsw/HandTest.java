161a508947c9eff3dbcd2c227636fa592496a9bd
Pier107 <p.creanza@protonmail.com>
2024-03-19 18:57:15 +0100

    Fix some tests

--- src/test/java/it/polimi/ingsw/HandTest.java
+++ src/test/java/it/polimi/ingsw/HandTest.java
@@ -1,28 +1,31 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.GameCard;
 import it.polimi.ingsw.model.Hand;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class HandTest {
 
     private Hand hand;
+    private GameCard card;
 
     @BeforeEach
     public void setup() {
+        card = mock(GameCard.class);
         hand = new Hand();
     }
 
     @Test
     public void testAddCard() {
-        GameCard card = new GameCard();
         hand.addCard(card);
         ArrayList<GameCard> cards = hand.getCards();
         assertEquals(1, cards.size());
@@ -31,7 +34,6 @@ public class HandTest {
 
     @Test
     public void testRemoveCard() {
-        GameCard card = new GameCard();
         hand.addCard(card);
         hand.removeCard(card);
         ArrayList<GameCard> cards = hand.getCards();
@@ -41,13 +43,13 @@ public class HandTest {
 
     @Test
     public void testHandFullException() {
-        GameCard card1 = new GameCard();
-        GameCard card2 = new GameCard();
-        GameCard card3 = new GameCard();
+        GameCard card1 = mock(GameCard.class);
+        GameCard card2 = mock(GameCard.class);
+        GameCard card3 = mock(GameCard.class);
         hand.addCard(card1);
         hand.addCard(card2);
         hand.addCard(card3);
-        assertThrows(IllegalArgumentException.class, () -> hand.addCard(new GameCard()));
+        assertThrows(IllegalArgumentException.class, () -> hand.addCard(card));
     }
 }
 
