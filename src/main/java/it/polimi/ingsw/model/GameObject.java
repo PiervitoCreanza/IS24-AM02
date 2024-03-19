d813cd0e8b0d7c21f1b8a036d51933acbab91514
marco <marcofebbo02@hotmail.it>
2024-03-19 14:02:06 +0100

    Remove collapsed Class

--- src/main/java/it/polimi/ingsw/model/GameObject.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.model;
-/**
- * Type of the Object that can be used
- */
-public enum GameObject {
-    QUILL,
-    INKWELL,
-    MANUSCRIPT
-}
--- src/main/java/it/polimi/ingsw/model/corner/EmptyCorner.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.model.corner;
-
-/**
- * Class for the Corner that contains nothing
- */
-
-public class EmptyCorner extends Corner{
-
-}
