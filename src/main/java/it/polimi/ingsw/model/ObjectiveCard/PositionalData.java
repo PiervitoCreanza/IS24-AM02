f1b7e3454f8c72245eccc7f5a58a3d41f453d7eb
Marco Febbo <115889695+Krema28@users.noreply.github.com>
2024-03-22 14:54:37 +0100

    Merge Review branch (#45)

--- src/main/java/it/polimi/ingsw/model/ObjectiveCard/PositionalData.java
+++ src/main/java/it/polimi/ingsw/model/ObjectiveCard/PositionalData.java
@@ -1,9 +1,9 @@
 package it.polimi.ingsw.model.ObjectiveCard;
 
 import it.polimi.ingsw.model.CardColor;
-import java.awt.*;
+import it.polimi.ingsw.model.Coordinate;
 
 /**
  * This is a supporter record used to represent the pattern contained in the target location card
  */
-public record PositionalData(Point point, CardColor cardColor) {}
+public record PositionalData(Coordinate coordinate, CardColor cardColor) {}
