f599d9c4b15e242e8d0686b16e4ba71f8b1d0f96
Marco Febbo <115889695+Krema28@users.noreply.github.com>
2024-03-22 14:54:37 +0100

    Merge Review branch (#45)

--- src/main/java/it/polimi/ingsw/model/Corner.java
+++ src/main/java/it/polimi/ingsw/model/Corner.java
@@ -1,45 +1,46 @@
 package it.polimi.ingsw.model;
 
-import java.util.Optional;
-
 /**
- * Abstract Class for define Corner of a GameCard Side
+ * This class represents a corner of a game card in the game.
+ * Each corner can be covered or uncovered and can contain a game item.
  */
-
 public class Corner {
-    protected boolean isCovered;
-    protected final GameItemEnum gameItem;
+    /**
+     * A boolean indicating whether the corner is covered or not.
+     */
+    private boolean isCovered;
 
+    /**
+     * The game item present in the corner.
+     */
+    private final GameItemEnum gameItem;
+
+    /**
+     * Constructs a new Corner object with the specified covered status and game item.
+     * @param isCovered A boolean indicating whether the corner is covered or not.
+     * @param gameItem The game item present in the corner.
+     */
     public Corner(boolean isCovered, GameItemEnum gameItem) {
         this.isCovered = isCovered;
         this.gameItem = gameItem;
     }
 
     /**
-     * This method returns the game item present in the corner.
-     * If the corner is covered, it returns NULL.
-     *
-     * @return the game item present in the corner or NULL
+     * Returns the game item present in the corner.
+     * If the corner is covered, it returns NONE.
+     * @return the game item present in the corner or NONE if the corner is covered.
      */
     public GameItemEnum getGameItem() {
-        return isCovered ? GameItemEnum.NONE : gameItem;
+        return this.isCovered ? GameItemEnum.NONE : this.gameItem;
     }
 
     /**
-     * This method sets the corner as covered.
+     * Sets the corner as covered and returns the game item that was present in the corner.
+     * Because when we set a corner as covered we lose the information about the game item that was present in the corner,
+     * @return The game item that was present in the corner.
      */
     public GameItemEnum setCovered() {
-        isCovered = true;
-        return gameItem;
+        this.isCovered = true;
+        return this.gameItem;
     }
-
-    /**
-     * This method checks if the corner exists (and get override by NonExistingCorner)
-     *
-     * @return true
-     */
-    public boolean isExisting() {
-        return true;
-    }
-
 }
--- src/main/java/it/polimi/ingsw/model/ObjectiveCard/PositionalObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/model/ObjectiveCard/PositionalObjectiveCard.java
@@ -1,57 +1,63 @@
 package it.polimi.ingsw.model.ObjectiveCard;
 
 import it.polimi.ingsw.model.CardColor;
+import it.polimi.ingsw.model.Coordinate;
 import it.polimi.ingsw.model.PlayerBoard;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
- * Class for the PositionalObjectiveCard that use position of the card to calculate the point
+ * This class extends the ObjectiveCard class and represents an objective card that rewards points based on the positions of the player's game cards.
+ * The points are calculated by checking if the player's game cards match the positional data required by the objective card.
  */
-
 public class PositionalObjectiveCard extends ObjectiveCard {
+    /**
+     * The positional data required by the objective card.
+     */
     private final ArrayList<PositionalData> positionalData;
 
+    /**
+     * Constructs a new PositionalObjectiveCard object with the specified number of points and the positional data required by the objective card.
+     * @param pointsWon The number of points the player can win by fulfilling the objective of this card.
+     * @param positionalData The positional data required by the objective card.
+     * @throws NullPointerException if positionalData is null.
+     */
     public PositionalObjectiveCard(int pointsWon, ArrayList<PositionalData> positionalData) {
         super(pointsWon);
+        Objects.requireNonNull(positionalData, "positionalData must be not null");
         this.positionalData = positionalData;
     }
 
     /**
      * This method calculates and returns the points won by the player.
      * It checks if the player's game cards match the positional data required by the objective card.
-     *
      * @param playerboard The player's game board.
      * @return The number of points won by the player.
      */
-
     @Override
     public int getPoints(PlayerBoard playerboard) {
-        int numOfMach = 0;                          //variable to count how many times player made the configuration
+        int numOfMatch = 0;                          //variable to count how many times player made the configuration
         CardColor firstColor = positionalData.get(0).cardColor();
-        ArrayList<Point> pointsCanMach = playerboard.getGameCards().stream()                  //Create a stream of GameCards
-                .filter(x -> x.getCardColor() == firstColor)            //Filtering by the color that I want
-                .map(x -> playerboard.getGameCardPosition(x).get())       //and at the end obtain the point of this card
+        ArrayList<Coordinate> coordinatesCanMatch = playerboard.getGameCards().stream()                  //Create a stream of GameCards
+                .filter(x -> x.getCardColor() == firstColor)                                             //Filtering by the color that I want
+                .map(x -> playerboard.getGameCardPosition(x).get())                                      //and at the end obtain the coordinate of this card
                 .collect(Collectors.toCollection(ArrayList::new));
 
-        HashSet<Point> pointsAlreadyUsed = new HashSet<>();             //List of points that are used for a mach
-
-        for (Point point : pointsCanMach) {                                 //For every possible point that is founded, try to follow the
-            ArrayList<Point> pointsMaybeUsed = new ArrayList<>();           //coordinate of Hashmap in positionRequired
-            //pointsMaybeUsed is an ArrayList that contains the points which, if correct,
-            // will be recorded in pointsAlreadyUsed
-            pointsMaybeUsed.add(point);
-            //Inizialization of pointsMaybeUsed
-            Point temp = new Point();
+        HashSet<Coordinate> coordinatesAlreadyUsed = new HashSet<>();           //List of coordinates that are used for a match
+        for (Coordinate coordinate : coordinatesCanMatch) {                     //For every possible coordinate that is found, try to follow the
+            ArrayList<Coordinate> coordinatesMaybeUsed = new ArrayList<>();     //coordinate of Hashmap in positionRequired
+            //coordinatesMaybeUsed is an ArrayList that contains the coordinates which, if correct,
+            // will be recorded in coordinatesAlreadyUsed
+            Coordinate temp = new Coordinate(0, 0);
             //Support variable
             for (PositionalData position : positionalData) {
-                temp.x = point.x + position.point().x;
-                temp.y = point.y + position.point().y;
+                temp.x = coordinate.x + position.coordinate().x;
+                temp.y = coordinate.y + position.coordinate().y;
                 //Move according to the coordinates marked on the hashmap
-                if (pointsAlreadyUsed.contains(temp)) {
+                if (coordinatesAlreadyUsed.contains(temp)) {
                     break;
                 }
                 if (playerboard.getGameCard(temp).isEmpty()) {
@@ -60,15 +66,14 @@ public class PositionalObjectiveCard extends ObjectiveCard {
                 if (playerboard.getGameCard(temp).get().getCardColor() != position.cardColor()) {
                     break;
                 }
-                pointsMaybeUsed.add(temp);
+                coordinatesMaybeUsed.add(temp);
             }
-            if (pointsMaybeUsed.size() == 3) {
-                pointsAlreadyUsed.addAll(pointsMaybeUsed);
-                numOfMach++;
+            if (coordinatesMaybeUsed.size() == 3) {
+                coordinatesAlreadyUsed.addAll(coordinatesMaybeUsed);
+                numOfMatch++;
             }
-            //Rispetto a point devo muovermi di positionRequired.get(1).
+            //Respect to coordinates I must move by positionRequired.get(1).
         }
-        return numOfMach * this.pointsWon;
+        return numOfMatch * this.pointsWon;
     }
 }
-
