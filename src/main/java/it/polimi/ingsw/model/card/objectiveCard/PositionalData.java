ef7cf2a94c5f9a248405356dc4086ae8dc367a09
Pier107 <p.creanza@protonmail.com>
2024-03-26 14:13:03 +0100

    Add missing dot

--- src/main/java/it/polimi/ingsw/model/card/objectiveCard/PositionalData.java
+++ src/main/java/it/polimi/ingsw/model/card/objectiveCard/PositionalData.java
@@ -4,7 +4,7 @@ import it.polimi.ingsw.model.card.CardColorEnum;
 import it.polimi.ingsw.model.utils.Coordinate;
 
 /**
- * This is a supporter record used to represent the pattern contained in the target location card
+ * This is a supporter record used to represent the pattern contained in the target location card.
  */
 public record PositionalData(Coordinate coordinate, CardColorEnum cardColorEnum) {
 }
