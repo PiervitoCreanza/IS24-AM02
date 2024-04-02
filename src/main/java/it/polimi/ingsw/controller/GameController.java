22a6084cf2d9246c21298cfdc52065f397b6c07f
simonecurci <simonefrancesco.curci@mail.polimi.it>
2024-04-02 22:41:29 +0200

    First simple implementation

---
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -0,0 +1,45 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.card.gameCard.GameCard;
+import it.polimi.ingsw.model.card.objectiveCard.ObjectiveCard;
+import it.polimi.ingsw.model.utils.Coordinate;
+
+public class GameController {
+    private final Game game;
+
+    public GameController(String gameName, int nPlayers, String playerName) {
+        this.game = new Game(gameName, nPlayers, playerName);
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void placeCard(String playerName, Coordinate coordinate, GameCard card) {
+        game.getPlayer(playerName).getPlayerBoard().setGameCard(coordinate, card);
+    }
+
+    public void drawCardFromField(String playerName, GameCard card) {
+        game.getGlobalBoard().drawCardFromField(card);
+        game.getPlayer(playerName).getPlayerHand().addCard(card);
+    }
+
+    public void drawCardFromResourceDeck(String playerName) {
+        GameCard drawnCard = game.getGlobalBoard().getResourceDeck().draw();
+        game.getPlayer(playerName).getPlayerHand().addCard(drawnCard);
+    }
+
+    public void drawCardFromGoldDeck(String playerName) {
+        GameCard drawnCard = game.getGlobalBoard().getGoldDeck().draw();
+        game.getPlayer(playerName).getPlayerHand().addCard(drawnCard);
+    }
+
+    public void switchCardSide(GameCard card) {
+        card.switchSide();
+    }
+
+    public void setPlayerObjective(String playerName, ObjectiveCard card) {
+        game.getPlayer(playerName).setPlayerObjective(card);
+    }
+}
