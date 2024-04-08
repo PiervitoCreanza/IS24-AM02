b4787aaf2fead097bbd00840233867ce832207bc
simonecurci <simonefrancesco.curci@mail.polimi.it>
2024-04-09 01:14:31 +0200

    Minor fixes

--- src/test/java/it/polimi/ingsw/model/card/objectiveCard/ItemObjectiveCardTest.java
+++ src/test/java/it/polimi/ingsw/model/card/objectiveCard/ItemObjectiveCardTest.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.model.card.objectiveCard;
 
 import it.polimi.ingsw.model.card.GameItemEnum;
-import it.polimi.ingsw.model.card.objectiveCard.ItemObjectiveCard;
 import it.polimi.ingsw.model.player.PlayerBoard;
 import it.polimi.ingsw.model.utils.store.GameItemStore;
 import org.junit.jupiter.api.DisplayName;
