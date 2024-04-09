9cc733dc3541293dbbf086ed346be4f70209a807
simonecurci <simonefrancesco.curci@mail.polimi.it>
2024-04-09 22:43:44 +0200

    Added javadoc and refactored code

--- src/main/java/it/polimi/ingsw/data/SideGameCardAdapter.java
+++ src/main/java/it/polimi/ingsw/data/SideGameCardAdapter.java
@@ -8,47 +8,84 @@ import it.polimi.ingsw.model.card.gameCard.front.goldCard.FrontGoldGameCard;
 import it.polimi.ingsw.model.card.gameCard.front.goldCard.FrontItemGoldGameCard;
 import it.polimi.ingsw.model.card.gameCard.front.goldCard.FrontPositionalGoldGameCard;
 import java.lang.reflect.Type;
-import java.util.Map;
 
+/**
+ * This class is an adapter for the SideGameCard class, providing methods for serializing and deserializing SideGameCard objects.
+ * It implements the JsonSerializer and JsonDeserializer interfaces from the Gson library.
+ */
 public class SideGameCardAdapter implements JsonSerializer<SideGameCard>, JsonDeserializer<SideGameCard> {
+
+    /**
+     * This method is used to deserialize a JSON element into a SideGameCard object.
+     * @param jsonElement The JSON element to deserialize.
+     * @param type The specific type of the object to deserialize to.
+     * @param jsonDeserializationContext Context for deserialization that is used to invoke default deserialization on the specified object.
+     * @return The deserialized SideGameCard object.
+     * @throws JsonParseException If the json cannot be parsed into a SideGameCard object.
+     */
     @Override
     public SideGameCard deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        // We convert the JsonElement to a JsonObject, we need it to get its properties
         JsonObject jsonObject = jsonElement.getAsJsonObject();
+        Class<?> sideTypeClass;
+        // We get the "sideType" property
         String sideType = jsonObject.get("sideType").getAsString();
-        jsonObject = jsonObject.get("sideContent").getAsJsonObject();
-        return switch (sideType) {
-            case "FrontGameCard" -> jsonDeserializationContext.deserialize(jsonObject, FrontGameCard.class);
-            case "FrontGoldGameCard" -> jsonDeserializationContext.deserialize(jsonObject, FrontGoldGameCard.class);
-            case "FrontItemGoldGameCard" ->
-                    jsonDeserializationContext.deserialize(jsonObject, FrontItemGoldGameCard.class);
-            case "FrontPositionalGoldGameCard" ->
-                    jsonDeserializationContext.deserialize(jsonObject, FrontPositionalGoldGameCard.class);
-            case "BackGameCard" -> jsonDeserializationContext.deserialize(jsonObject, BackGameCard.class);
-            default -> null;
-        };
+        // We decide which is the class type of the side
+        switch (sideType) {
+            case "FrontGameCard" -> sideTypeClass = FrontGameCard.class;
+            case "FrontGoldGameCard" -> sideTypeClass = FrontGoldGameCard.class;
+            case "FrontItemGoldGameCard" -> sideTypeClass = FrontItemGoldGameCard.class;
+            case "FrontPositionalGoldGameCard" -> sideTypeClass = FrontPositionalGoldGameCard.class;
+            case "BackGameCard" -> sideTypeClass = BackGameCard.class;
+            default -> throw new JsonParseException("sideType property is missing or wrong");
+        }
+        // We deserialize the "sideContent" with the class type found above and return it
+        return jsonDeserializationContext.deserialize(jsonObject.get("sideContent"), sideTypeClass);
     }
 
+    /**
+     * This method is used to serialize a SideGameCard object into a JSON element.
+     * @param sideGameCard The SideGameCard object to serialize.
+     * @param type The specific type of the object to serialize from.
+     * @param jsonSerializationContext Context for serialization that is used to invoke default serialization on the specified object.
+     * @return The serialized JSON element.
+     * @throws RuntimeException If the sideGameCard object is of unknown class
+     */
     @Override
     public JsonElement serialize(SideGameCard sideGameCard, Type type, JsonSerializationContext jsonSerializationContext) {
+        // We create a new JsonObject, it will contain the serialized sideGameCard
         JsonObject jsonObject = new JsonObject();
-        JsonElement serializedSideGameCard = null;
-        if (sideGameCard instanceof FrontPositionalGoldGameCard) {
-            jsonObject.addProperty("sideType", "FrontPositionalGoldGameCard");
-            serializedSideGameCard = jsonSerializationContext.serialize(sideGameCard, FrontPositionalGoldGameCard.class);
-        } else if (sideGameCard instanceof FrontItemGoldGameCard) {
-            jsonObject.addProperty("sideType", "FrontItemGoldGameCard");
-            serializedSideGameCard = jsonSerializationContext.serialize(sideGameCard, FrontItemGoldGameCard.class);
-        } else if (sideGameCard instanceof FrontGoldGameCard) {
-            jsonObject.addProperty("sideType", "FrontGoldGameCard");
-            serializedSideGameCard = jsonSerializationContext.serialize(sideGameCard, FrontGoldGameCard.class);
-        } else if (sideGameCard instanceof FrontGameCard) {
-            jsonObject.addProperty("sideType", "FrontGameCard");
-            serializedSideGameCard = jsonSerializationContext.serialize(sideGameCard, FrontGameCard.class);
-        } else if (sideGameCard instanceof BackGameCard) {
-            jsonObject.addProperty("sideType", "BackGameCard");
-            serializedSideGameCard = jsonSerializationContext.serialize(sideGameCard, BackGameCard.class);
+        Class<?> sideTypeClass;
+        String sideType;
+        // We check the class of sideGameCard and set the property and class accordingly
+        switch (sideGameCard) {
+            case FrontPositionalGoldGameCard ignored -> {
+                sideType = "FrontPositionalGoldGameCard";
+                sideTypeClass = FrontPositionalGoldGameCard.class;
+            }
+            case FrontItemGoldGameCard ignored -> {
+                sideType = "FrontItemGoldGameCard";
+                sideTypeClass = FrontItemGoldGameCard.class;
+            }
+            case FrontGoldGameCard ignored -> {
+                sideType = "FrontGoldGameCard";
+                sideTypeClass = FrontGoldGameCard.class;
+            }
+            case FrontGameCard ignored -> {
+                sideType = "FrontGameCard";
+                sideTypeClass = FrontGameCard.class;
+            }
+            case BackGameCard ignored -> {
+                sideType = "BackGameCard";
+                sideTypeClass = BackGameCard.class;
+            }
+            default -> throw new RuntimeException("sideGameCard is of unknown class");
         }
-        jsonObject.add("sideContent", serializedSideGameCard);
+        // We add the property to the JsonObject
+        jsonObject.addProperty("sideType", sideType);
+        // We serialize the sideGameCard with the chosen class and add it in the sideContent property
+        jsonObject.add("sideContent", jsonSerializationContext.serialize(sideGameCard, sideTypeClass));
+        // We return the serialized sideGameCard
         return jsonObject;
     }
 }
