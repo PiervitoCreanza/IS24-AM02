4d8421d2c7570ba249c79b2a8111d95ef8b7bcad
Pier107 <p.creanza@protonmail.com>
2024-04-16 15:05:56 +0200

    Fix comments requests

--- src/test/java/it/polimi/ingsw/controller/GameControllerMiddlewareTest.java
+++ src/test/java/it/polimi/ingsw/controller/GameControllerMiddlewareTest.java
@@ -2,14 +2,14 @@ package it.polimi.ingsw.controller;
 
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.player.Player;
-import jdk.jfr.Description;
-import jdk.jfr.Name;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -39,6 +39,14 @@ class GameControllerMiddlewareTest {
             }
         }).when(game).setNextPlayer();
         when(game.getCurrentPlayerIndex()).thenAnswer(invocation -> currentPlayerIndex);
+        Player player0 = Mockito.mock(Player.class);
+        when(player0.getPlayerName()).thenReturn("player0");
+        Player player1 = Mockito.mock(Player.class);
+        when(player1.getPlayerName()).thenReturn("player1");
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(player0);
+        players.add(player1);
+        when(game.getPlayers()).thenAnswer(invocation -> players);
     }
 
     @Test
@@ -64,14 +72,11 @@ class GameControllerMiddlewareTest {
     }
 
     @Test
-    @DisplayName("Test if a card can be placed when gameStatus is PLACE_CARD or INIT_PLACE_STARTER_CARD")
+    @DisplayName("Test if a card can be placed when gameStatus is PLACE_CARD")
     void placeCard1() {
         gameControllerMiddleware.setGameStatus(GameStatusEnum.PLACE_CARD);
         gameControllerMiddleware.placeCard("player0", null, null);
         assertEquals(GameStatusEnum.DRAW_CARD, gameControllerMiddleware.getGameStatus());
-        gameControllerMiddleware.setGameStatus(GameStatusEnum.INIT_PLACE_STARTER_CARD);
-        gameControllerMiddleware.placeCard("player0", null, null);
-        assertEquals(GameStatusEnum.INIT_DRAW_CARD, gameControllerMiddleware.getGameStatus());
     }
 
     @Test
@@ -92,29 +97,10 @@ class GameControllerMiddlewareTest {
     }
 
     @Test
-    @DisplayName("Test if a player can draw only 2 resource cards during initialization")
-    void drawCardFromResourceDeck() {
-        gameControllerMiddleware.setGameStatus(GameStatusEnum.INIT_DRAW_CARD);
-        gameControllerMiddleware.drawCardFromResourceDeck("player0");
-        gameControllerMiddleware.drawCardFromResourceDeck("player0");
-        assertThrows(IllegalStateException.class, () -> gameControllerMiddleware.drawCardFromResourceDeck("player0"));
-    }
-
-    @Test
-    @DisplayName("Test if a player can draw only 1 gold card during initialization")
-    void drawCardFromGoldDeck() {
-        gameControllerMiddleware.setGameStatus(GameStatusEnum.INIT_DRAW_CARD);
-        gameControllerMiddleware.drawCardFromGoldDeck("player0");
-        assertThrows(IllegalStateException.class, () -> gameControllerMiddleware.drawCardFromGoldDeck("player0"));
-    }
-
-    @Test
-    @DisplayName("Test if the game status changes to INIT_CHOOSE_OBJECTIVE_CARD after drawing 2 resource cards and 1 gold card\"")
+    @DisplayName("Test if the game status changes to INIT_CHOOSE_OBJECTIVE_CARD after placing the starter card\"")
     void drawCardFromDeck() {
-        gameControllerMiddleware.setGameStatus(GameStatusEnum.INIT_DRAW_CARD);
-        gameControllerMiddleware.drawCardFromResourceDeck("player0");
-        gameControllerMiddleware.drawCardFromResourceDeck("player0");
-        gameControllerMiddleware.drawCardFromGoldDeck("player0");
+        gameControllerMiddleware.setGameStatus(GameStatusEnum.INIT_PLACE_STARTER_CARD);
+        gameControllerMiddleware.placeCard("player0", null, null);
         assertEquals(GameStatusEnum.INIT_CHOOSE_OBJECTIVE_CARD, gameControllerMiddleware.getGameStatus());
     }
 
@@ -165,30 +151,22 @@ class GameControllerMiddlewareTest {
         // -- Player 0 can't draw cards yet
         assertThrows(IllegalStateException.class, () -> gameControllerMiddleware.drawCardFromResourceDeck("player0"));
 
+        // Player 0 can switch the starter card side
+        gameControllerMiddleware.switchCardSide("player0", null);
+
         // Player 0 places his starter card
         gameControllerMiddleware.placeCard("player0", null, null);
         // Player 1 can't place his starter card yet
         assertThrows(IllegalStateException.class, () -> gameControllerMiddleware.placeCard("player1", null, null));
 
-        // Player 0 can't place his starter card anymore
+        // Player 0 places his starter card
+        gameControllerMiddleware.setPlayerObjective("player0", null);
+
+        // Player 0 can't place his objective card anymore
         assertThrows(IllegalStateException.class, () -> gameControllerMiddleware.placeCard("player0", null, null));
 
-        // Player 0 can't choose his objective card yet
+        // Player 0 can't choose his objective card anymore
         assertThrows(IllegalStateException.class, () -> gameControllerMiddleware.setPlayerObjective("player0", null));
-
-        // Player 0 draws 2 resource cards and 1 gold card
-        gameControllerMiddleware.drawCardFromResourceDeck("player0");
-        // -- Player 1 can't draw cards yet
-        assertThrows(IllegalStateException.class, () -> gameControllerMiddleware.drawCardFromResourceDeck("player1"));
-        gameControllerMiddleware.drawCardFromResourceDeck("player0");
-        // -- Player 1 can't draw cards yet
-        assertThrows(IllegalStateException.class, () -> gameControllerMiddleware.drawCardFromResourceDeck("player1"));
-        gameControllerMiddleware.drawCardFromGoldDeck("player0");
-        // -- Player 1 can't draw cards yet
-        assertThrows(IllegalStateException.class, () -> gameControllerMiddleware.drawCardFromGoldDeck("player1"));
-
-        // Player 0 chooses his objective card
-        gameControllerMiddleware.setPlayerObjective("player0", null);
 
         assertEquals(GameStatusEnum.INIT_PLACE_STARTER_CARD, gameControllerMiddleware.getGameStatus());
         // -- Player 1 can't draw cards yet
@@ -198,10 +176,7 @@ class GameControllerMiddlewareTest {
         gameControllerMiddleware.placeCard("player1", null, null);
         // Player 0 can't place his starter card
         assertThrows(IllegalStateException.class, () -> gameControllerMiddleware.placeCard("player0", null, null));
-        // Player 1 draws 2 resource cards and 1 gold card
-        gameControllerMiddleware.drawCardFromResourceDeck("player1");
-        gameControllerMiddleware.drawCardFromResourceDeck("player1");
-        gameControllerMiddleware.drawCardFromGoldDeck("player1");
+
         // Player 1 chooses his objective card
         gameControllerMiddleware.setPlayerObjective("player1", null);
 
@@ -222,12 +197,17 @@ class GameControllerMiddlewareTest {
         when(game.isLastRound()).thenReturn(true);
         // Player 2 can draw a card
         gameControllerMiddleware.drawCardFromField("player1", null);
-        // It is the last round
+        // It is the last round phase. We have to play
         // Player 0 can place a card
         gameControllerMiddleware.placeCard("player0", null, null);
-        // Player 1 can draw a card
+        // Player 0 can draw a card
         gameControllerMiddleware.drawCardFromField("player0", null);
+        // Player 1 can place a card
+        gameControllerMiddleware.placeCard("player1", null, null);
+        // Player 1 can place a card
+        gameControllerMiddleware.drawCardFromField("player1", null);
         // Game is over
         assertEquals(GameStatusEnum.GAME_OVER, gameControllerMiddleware.getGameStatus());
     }
+    // TODO: Add test for game end with 3 or 4 players.
 }
