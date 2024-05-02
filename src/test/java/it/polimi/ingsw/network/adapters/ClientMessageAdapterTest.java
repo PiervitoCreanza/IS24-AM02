f04d4e55ee44ccfd8b10e2420aea712453f87cf0
marco <marcofebbo02@hotmail.it>
2024-05-03 00:00:28 +0200

    Small fixes

--- src/test/java/it/polimi/ingsw/network/adapters/ClientMessageAdapterTest.java
+++ src/test/java/it/polimi/ingsw/network/adapters/ClientMessageAdapterTest.java
@@ -1,11 +1,15 @@
 package it.polimi.ingsw.network.adapters;
 
-import it.polimi.ingsw.data.Parser;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import it.polimi.ingsw.data.ObjectiveCardAdapter;
+import it.polimi.ingsw.data.SideGameCardAdapter;
 import it.polimi.ingsw.model.card.CardColorEnum;
 import it.polimi.ingsw.model.card.GameItemEnum;
 import it.polimi.ingsw.model.card.corner.Corner;
 import it.polimi.ingsw.model.card.gameCard.BackGameCard;
 import it.polimi.ingsw.model.card.gameCard.GameCard;
+import it.polimi.ingsw.model.card.gameCard.SideGameCard;
 import it.polimi.ingsw.model.card.gameCard.front.FrontGameCard;
 import it.polimi.ingsw.model.card.objectiveCard.ItemObjectiveCard;
 import it.polimi.ingsw.model.card.objectiveCard.ObjectiveCard;
@@ -14,6 +18,9 @@ import it.polimi.ingsw.model.card.objectiveCard.PositionalObjectiveCard;
 import it.polimi.ingsw.model.utils.Coordinate;
 import it.polimi.ingsw.model.utils.store.GameItemStore;
 import it.polimi.ingsw.network.client.message.ClientMessage;
+import it.polimi.ingsw.network.client.message.CreateGameClientMessage;
+import it.polimi.ingsw.network.client.message.GetGamesClientMessage;
+import it.polimi.ingsw.network.client.message.JoinGameClientMessage;
 import it.polimi.ingsw.network.client.message.gameController.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,17 +29,22 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("ClientMessageAdapterTest")
 public class ClientMessageAdapterTest {
-    Parser parser = new Parser();
+    Gson gson = new GsonBuilder()
+            .registerTypeAdapter(SideGameCard.class, new SideGameCardAdapter())
+            .registerTypeAdapter(ObjectiveCard.class, new ObjectiveCardAdapter())
+            .registerTypeAdapter(ClientMessage.class, new ClientMessageAdapter())
+            .create();
 
     @Test
     @DisplayName("Test if serialization and deserialization of GetGamesClientMessage works")
     public void serializeAndDeserializeGetGamesClientMessage() {
         ClientMessage getGamesClientMessage = new GetGamesClientMessage();
         // Serialize
-        String jsonGetGamesClientMessage = this.parser.serializeToJson(getGamesClientMessage);
+        String jsonGetGamesClientMessage = this.gson.toJson(getGamesClientMessage);
         // Deserialize
-        assertEquals(getGamesClientMessage, this.parser.deserializeFromJson(jsonGetGamesClientMessage, ClientMessage.class));
+        assertEquals(getGamesClientMessage, this.gson.fromJson(jsonGetGamesClientMessage, ClientMessage.class));
     }
 
     @Test
@@ -40,9 +52,9 @@ public class ClientMessageAdapterTest {
     public void serializeAndDeserializeCreateGameClientMessage() {
         ClientMessage createGameClientMessage = new CreateGameClientMessage("Game1", 3, "Player1");
         // Serialize
-        String jsonCreateGameClientMessage = this.parser.serializeToJson(createGameClientMessage);
+        String jsonCreateGameClientMessage = this.gson.toJson(createGameClientMessage);
         // Deserialize
-        assertEquals(createGameClientMessage, this.parser.deserializeFromJson(jsonCreateGameClientMessage, ClientMessage.class));
+        assertEquals(createGameClientMessage, this.gson.fromJson(jsonCreateGameClientMessage, ClientMessage.class));
     }
 
     @Test
@@ -50,9 +62,9 @@ public class ClientMessageAdapterTest {
     public void serializeAndDeserializeJoinGameClientMessage() {
         ClientMessage joinGameClientMessage = new JoinGameClientMessage("Game1", "Player1");
         // Serialize
-        String jsonJoinGameClientMessage = this.parser.serializeToJson(joinGameClientMessage);
+        String jsonJoinGameClientMessage = this.gson.toJson(joinGameClientMessage);
         // Deserialize
-        assertEquals(joinGameClientMessage, this.parser.deserializeFromJson(jsonJoinGameClientMessage, ClientMessage.class));
+        assertEquals(joinGameClientMessage, this.gson.fromJson(jsonJoinGameClientMessage, ClientMessage.class));
     }
 
     @Test
@@ -60,9 +72,9 @@ public class ClientMessageAdapterTest {
     public void serializeAndDeserializeChoosePlayerColorClientMessage() {
         ClientMessage choosePlayerColorClientMessage = new ChoosePlayerColorClientMessage("Red", "Game1", "Player1");
         // Serialize
-        String jsonChoosePlayerColorClientMessage = this.parser.serializeToJson(choosePlayerColorClientMessage);
+        String jsonChoosePlayerColorClientMessage = this.gson.toJson(choosePlayerColorClientMessage);
         // Deserialize
-        assertEquals(choosePlayerColorClientMessage, this.parser.deserializeFromJson(jsonChoosePlayerColorClientMessage, ClientMessage.class));
+        assertEquals(choosePlayerColorClientMessage, this.gson.fromJson(jsonChoosePlayerColorClientMessage, ClientMessage.class));
     }
 
     @Test
@@ -73,9 +85,9 @@ public class ClientMessageAdapterTest {
         ObjectiveCard objectiveCard1 = new ItemObjectiveCard(1, 2, multiplier);
         ClientMessage setPlayerObjectiveClientMessage1 = new SetPlayerObjectiveClientMessage("Game1", "Player1", objectiveCard1);
         // Serialize
-        String jsonSetPlayerObjectiveClientMessage1 = this.parser.serializeToJson(setPlayerObjectiveClientMessage1);
+        String jsonSetPlayerObjectiveClientMessage1 = this.gson.toJson(setPlayerObjectiveClientMessage1);
         // Deserialize
-        assertEquals(setPlayerObjectiveClientMessage1, this.parser.deserializeFromJson(jsonSetPlayerObjectiveClientMessage1, ClientMessage.class));
+        assertEquals(setPlayerObjectiveClientMessage1, this.gson.fromJson(jsonSetPlayerObjectiveClientMessage1, ClientMessage.class));
         ArrayList<PositionalData> positionalDataArrayList = new ArrayList<>();
         positionalDataArrayList.add(new PositionalData(new Coordinate(1, 1), CardColorEnum.RED));
         positionalDataArrayList.add(new PositionalData(new Coordinate(1, 2), CardColorEnum.BLUE));
@@ -83,9 +95,9 @@ public class ClientMessageAdapterTest {
         ObjectiveCard objectiveCard2 = new PositionalObjectiveCard(1, 2, positionalDataArrayList);
         ClientMessage setPlayerObjectiveClientMessage2 = new SetPlayerObjectiveClientMessage("Game2", "Player2", objectiveCard2);
         // Serialize
-        String jsonSetPlayerObjectiveClientMessage2 = this.parser.serializeToJson(setPlayerObjectiveClientMessage2);
+        String jsonSetPlayerObjectiveClientMessage2 = this.gson.toJson(setPlayerObjectiveClientMessage2);
         // Deserialize
-        assertEquals(setPlayerObjectiveClientMessage2, this.parser.deserializeFromJson(jsonSetPlayerObjectiveClientMessage2, ClientMessage.class));
+        assertEquals(setPlayerObjectiveClientMessage2, this.gson.fromJson(jsonSetPlayerObjectiveClientMessage2, ClientMessage.class));
     }
 
     @Test
@@ -97,9 +109,9 @@ public class ClientMessageAdapterTest {
 
         ClientMessage placeCardClientMessage = new PlaceCardClientMessage("Game1", "Player1", new Coordinate(1, 1), resourceGameCard);
         // Serialize
-        String jsonPlaceCardClientMessage = this.parser.serializeToJson(placeCardClientMessage);
+        String jsonPlaceCardClientMessage = this.gson.toJson(placeCardClientMessage);
         // Deserialize
-        assertEquals(placeCardClientMessage, this.parser.deserializeFromJson(jsonPlaceCardClientMessage, ClientMessage.class));
+        assertEquals(placeCardClientMessage, this.gson.fromJson(jsonPlaceCardClientMessage, ClientMessage.class));
     }
 
     @Test
@@ -111,9 +123,9 @@ public class ClientMessageAdapterTest {
 
         ClientMessage drawCardFromFieldClientMessage = new DrawCardFromFieldClientMessage("Game1", "Player1", resourceGameCard);
         // Serialize
-        String jsonDrawCardFromFieldClientMessage = this.parser.serializeToJson(drawCardFromFieldClientMessage);
+        String jsonDrawCardFromFieldClientMessage = this.gson.toJson(drawCardFromFieldClientMessage);
         // Deserialize
-        assertEquals(drawCardFromFieldClientMessage, this.parser.deserializeFromJson(jsonDrawCardFromFieldClientMessage, ClientMessage.class));
+        assertEquals(drawCardFromFieldClientMessage, this.gson.fromJson(jsonDrawCardFromFieldClientMessage, ClientMessage.class));
     }
 
     @Test
@@ -121,9 +133,9 @@ public class ClientMessageAdapterTest {
     public void serializeAndDeserializeDrawCardFromGoldDeckClientMessage() {
         ClientMessage drawCardFromGoldDeckClientMessage = new DrawCardFromGoldDeckClientMessage("Game1", "Player1");
         // Serialize
-        String jsonDrawCardFromGoldDeckClientMessage = this.parser.serializeToJson(drawCardFromGoldDeckClientMessage);
+        String jsonDrawCardFromGoldDeckClientMessage = this.gson.toJson(drawCardFromGoldDeckClientMessage);
         // Deserialize
-        assertEquals(drawCardFromGoldDeckClientMessage, this.parser.deserializeFromJson(jsonDrawCardFromGoldDeckClientMessage, ClientMessage.class));
+        assertEquals(drawCardFromGoldDeckClientMessage, this.gson.fromJson(jsonDrawCardFromGoldDeckClientMessage, ClientMessage.class));
     }
 
     @Test
@@ -131,9 +143,9 @@ public class ClientMessageAdapterTest {
     public void serializeAndDeserializeDrawCardFromResourceDeckClientMessage() {
         ClientMessage drawCardFromResourceDeckClientMessage = new DrawCardFromResourceDeckClientMessage("Game1", "Player1");
         // Serialize
-        String jsonDrawCardFromResourceDeckClientMessage = this.parser.serializeToJson(drawCardFromResourceDeckClientMessage);
+        String jsonDrawCardFromResourceDeckClientMessage = this.gson.toJson(drawCardFromResourceDeckClientMessage);
         // Deserialize
-        assertEquals(drawCardFromResourceDeckClientMessage, this.parser.deserializeFromJson(jsonDrawCardFromResourceDeckClientMessage, ClientMessage.class));
+        assertEquals(drawCardFromResourceDeckClientMessage, this.gson.fromJson(jsonDrawCardFromResourceDeckClientMessage, ClientMessage.class));
     }
 
     @Test
@@ -145,8 +157,8 @@ public class ClientMessageAdapterTest {
 
         ClientMessage switchCardSideClientMessage = new SwitchCardSideClientMessage("Game1", "Player1", resourceGameCard);
         // Serialize
-        String jsonSwitchCardSideClientMessage = this.parser.serializeToJson(switchCardSideClientMessage);
+        String jsonSwitchCardSideClientMessage = this.gson.toJson(switchCardSideClientMessage);
         // Deserialize
-        assertEquals(switchCardSideClientMessage, this.parser.deserializeFromJson(jsonSwitchCardSideClientMessage, ClientMessage.class));
+        assertEquals(switchCardSideClientMessage, this.gson.fromJson(jsonSwitchCardSideClientMessage, ClientMessage.class));
     }
 }
