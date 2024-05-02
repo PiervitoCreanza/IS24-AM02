30473dc6c32a43835d6f9b3200d7ca75fb2a6817
marco <marcofebbo02@hotmail.it>
2024-05-02 12:31:39 +0200

    Add JavaDoc

--- src/main/java/it/polimi/ingsw/network/client/message/gameController/PlaceCardClientMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/message/gameController/PlaceCardClientMessage.java
@@ -4,20 +4,50 @@ import it.polimi.ingsw.model.card.gameCard.GameCard;
 import it.polimi.ingsw.model.utils.Coordinate;
 import it.polimi.ingsw.network.client.message.PlayerActionEnum;
 
+/**
+ * This class extends the InGameClientMessage class and represents a specific type of in-game client message: a request to place a card.
+ * It contains the name of the game, the name of the player who is placing the card, the coordinate where the card is to be placed, and the game card to be placed.
+ */
 public class PlaceCardClientMessage extends InGameClientMessage {
+    /**
+     * The coordinate where the card is to be placed.
+     */
     private final Coordinate coordinate;
+
+    /**
+     * The game card to be placed.
+     */
     private final GameCard card;
 
+    /**
+     * Constructor for PlaceCardClientMessage.
+     * Initializes the player action with the PLACECARD value, and sets the game name, player name, coordinate, and game card.
+     *
+     * @param gameName   The name of the game. This cannot be null.
+     * @param playerName The name of the player who is placing the card. This cannot be null.
+     * @param coordinate The coordinate where the card is to be placed. This cannot be null.
+     * @param card       The game card to be placed. This cannot be null.
+     */
     public PlaceCardClientMessage(String gameName, String playerName, Coordinate coordinate, GameCard card) {
         super(PlayerActionEnum.PLACECARD, gameName, playerName);
         this.coordinate = coordinate;
         this.card = card;
     }
 
+    /**
+     * Returns the coordinate where the card is to be placed.
+     *
+     * @return The coordinate where the card is to be placed.
+     */
     public Coordinate getCoordinate() {
         return coordinate;
     }
 
+    /**
+     * Returns the game card to be placed.
+     *
+     * @return The game card to be placed.
+     */
     public GameCard getCard() {
         return card;
     }
--- src/main/java/it/polimi/ingsw/network/client/message/gameController/SwitchCardSideClientMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/message/gameController/SwitchCardSideClientMessage.java
@@ -3,14 +3,34 @@ package it.polimi.ingsw.network.client.message.gameController;
 import it.polimi.ingsw.model.card.gameCard.GameCard;
 import it.polimi.ingsw.network.client.message.PlayerActionEnum;
 
+/**
+ * This class extends the InGameClientMessage class and represents a specific type of in-game client message: a request to switch the side of a card.
+ * It contains the name of the game, the name of the player who is switching the card side, and the game card to be switched.
+ */
 public class SwitchCardSideClientMessage extends InGameClientMessage {
+    /**
+     * The game card to be switched.
+     */
     private final GameCard gameCard;
 
+    /**
+     * Constructor for SwitchCardSideClientMessage.
+     * Initializes the player action with the SWITCHCARDSIDE value, and sets the game name, player name, and game card.
+     *
+     * @param gameName   The name of the game. This cannot be null.
+     * @param playerName The name of the player who is switching the card side. This cannot be null.
+     * @param gameCard   The game card to be switched. This cannot be null.
+     */
     public SwitchCardSideClientMessage(String gameName, String playerName, GameCard gameCard) {
         super(PlayerActionEnum.SWITCHCARDSIDE, gameName, playerName);
         this.gameCard = gameCard;
     }
 
+    /**
+     * Returns the game card to be switched.
+     *
+     * @return The game card to be switched.
+     */
     public GameCard getGameCard() {
         return gameCard;
     }
