9c1b174f448ef4aff1ffd06166951d5a5937add4
simonecurci <simonefrancesco.curci@mail.polimi.it>
2024-05-10 22:20:45 +0200

    Minor fix

--- src/main/java/it/polimi/ingsw/tui/draw/cards/objectiveCard/ObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/tui/draw/cards/objectiveCard/ObjectiveCard.java
@@ -5,5 +5,18 @@ import it.polimi.ingsw.tui.draw.DrawArea;
 public class ObjectiveCard {
     private final DrawArea drawArea;
 
+    public ObjectiveCard(ObjectiveCard objectiveCard) {
+        drawArea = new DrawArea(
+                """
+                        ┌─────────────────────┐
+                        │    ---              │
+                        │  ┌─────┐            │
+                        │  │  2  │            │
+                        │  └─────┘            │
+                        │    ---              │
+                        └─────────────────────┘
+                        """
+        );
+    }
 
 }
