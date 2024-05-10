a914bdf34b4ee26aba310fbdcc8c4604fd4b405a
Pier107 <p.creanza@protonmail.com>
2024-05-10 21:40:20 +0200

    Various edits

--- src/main/java/it/polimi/ingsw/tui/draw/cards/objectiveCard/ObjectiveCardComponent.java
+++ src/main/java/it/polimi/ingsw/tui/draw/cards/objectiveCard/ObjectiveCardComponent.java
@@ -1,13 +1,16 @@
-package it.polimi.ingsw.tui.draw;
+package it.polimi.ingsw.tui.draw.cards.objectiveCard;
 
 import it.polimi.ingsw.model.card.CardColorEnum;
 import it.polimi.ingsw.model.card.objectiveCard.PositionalData;
 import it.polimi.ingsw.model.card.objectiveCard.PositionalObjectiveCard;
 import it.polimi.ingsw.model.utils.Coordinate;
+import it.polimi.ingsw.tui.draw.DrawArea;
+import it.polimi.ingsw.tui.draw.Drawable;
 import it.polimi.ingsw.tui.utils.ColorsEnum;
 import it.polimi.ingsw.tui.utils.Pair;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 /*
                 ┌───────────────┐
@@ -24,10 +27,10 @@ public class ObjectiveCardComponent implements Drawable {
         drawArea = new DrawArea(
                 """
                         ┌───────────────┐
-                        │  ┌───┐        │
-                        │  │   │        │
+                        │| ┌───┐        │
+                        │| │   │        │
                         │  └───┘        │
-                        │               │
+                        │   ---         │
                         └───────────────┘
                         """
         );
@@ -35,23 +38,24 @@ public class ObjectiveCardComponent implements Drawable {
         drawArea.drawAt(5, 2, objectiveCard.getPointsWon(), ColorsEnum.RED);
         ArrayList<PositionalData> pd = objectiveCard.getPositionalData();
 
-        int positionalAnchorX = 12;
+        int positionalAnchorX = 10;
         int positionalAnchorY = 1;
         ArrayList<Pair<Coordinate, CardColorEnum>> pair = new ArrayList<>();
         for (PositionalData p : pd) {
             pair.add(new Pair<>(p.coordinate(), p.cardColorEnum()));
         }
         pair = drawArea.convertCoordinates(pair);
-        ArrayList<Pair<Coordinate, CardColorEnum>> finalPair = pair;
-        pair.forEach((p) -> {
+        pair.sort(Comparator.comparingInt(p -> p.key().y));
+        int extraSpaceY = 0;
+        for (Pair<Coordinate, CardColorEnum> p : pair) {
+            System.out.println(p.key().x + " " + p.key().y + " " + p.value());
             // Remove empty rows
-            if (finalPair.stream().anyMatch(tp -> tp.key().x == p.key().x && tp.key().y == p.key().y + 2) && finalPair.stream().noneMatch(tp -> tp.key().y == p.key().y + 1)) {
-                drawArea.drawAt(positionalAnchorX + p.key().x, positionalAnchorY + 1 + p.key().y, "■", p.value().getColor());
-            } else {
-                drawArea.drawAt(positionalAnchorX + p.key().x, positionalAnchorY + p.key().y, "■", p.value().getColor());
+            if (pair.stream().noneMatch(tp -> p.key().y == tp.key().y + 1) && p.key().y != 0) {
+                extraSpaceY += 1;
             }
+            drawArea.drawAt(positionalAnchorX + p.key().x * 2, positionalAnchorY + p.key().y - extraSpaceY, "■", p.value().getColor());
 
-        });
+        }
 
     }
 
--- src/test/java/it/polimi/ingsw/tui/draw/ObjectiveCardComponentTest.java
+++ src/test/java/it/polimi/ingsw/tui/draw/ObjectiveCardComponentTest.java
@@ -2,13 +2,14 @@ package it.polimi.ingsw.tui.draw;
 
 import it.polimi.ingsw.data.Parser;
 import it.polimi.ingsw.model.card.objectiveCard.PositionalObjectiveCard;
+import it.polimi.ingsw.tui.draw.cards.objectiveCard.ObjectiveCardComponent;
 import org.junit.jupiter.api.Test;
 
 class ObjectiveCardComponentTest {
 
     @Test
     void getDrawArea() {
-        PositionalObjectiveCard objectiveCard = (PositionalObjectiveCard) new Parser().getObjectiveDeck().getCards().stream().filter(c -> c.getCardId() == 91).findFirst().get();
+        PositionalObjectiveCard objectiveCard = (PositionalObjectiveCard) new Parser().getObjectiveDeck().getCards().stream().filter(c -> c.getCardId() == 92).findFirst().get();
         ObjectiveCardComponent objectiveCardComponent = new ObjectiveCardComponent(objectiveCard);
         System.out.println(objectiveCard.getClass());
         System.out.println(objectiveCardComponent);
