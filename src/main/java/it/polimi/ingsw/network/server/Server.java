a4126a69692c3bb06467de4023cbff40be7ab55e
mattiacolombomc <info@mattiacolombo.com>
2024-05-12 12:17:21 +0200

    Fixed fallback to default ports

--- src/main/java/it/polimi/ingsw/network/server/Server.java
+++ src/main/java/it/polimi/ingsw/network/server/Server.java
@@ -8,14 +8,19 @@ import it.polimi.ingsw.tui.utils.Utils;
 import org.apache.commons.cli.*;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+
+import static it.polimi.ingsw.tui.utils.Utils.ANSI_PURPLE;
+import static it.polimi.ingsw.tui.utils.Utils.ANSI_RESET;
 
 /**
  * This class is responsible for setting up and running the TCP and RMI servers.
@@ -28,6 +33,10 @@ public class Server {
      * @param args the input arguments. If a single argument is provided, it is used as the port number for the server.
      */
     public static long HEARTBEAT_TIMEOUT = 2500; //Static Instance of the timeout for the heartbeat
+    public static boolean IS_DEBUG = false; //Static Instance of the timeout for the heartbeat
+
+    private static int TCPPortNumber;
+    private static int RMIPortNumber;
     public static void main(String[] args) throws RemoteException {
         /* ***************************************
          * INSTANTIATE MAIN CONTROLLER and NETWORK COMMAND MAPPER
@@ -39,12 +48,13 @@ public class Server {
          * CLI ARGUMENTS PARSING
          * ***************************************/
         CommandLine cmd = parseCommandLineArgs(args);
-        int TCPPortNumber = Integer.parseInt(cmd.getOptionValue("TCP_P", "12345")); // default is 12345
-        int RMIPortNumber = Integer.parseInt(cmd.getOptionValue("RMI_P", "1099"));
+        TCPPortNumber = Integer.parseInt(cmd.getOptionValue("TCP_P", "12345")); // default is 12345
+        RMIPortNumber = Integer.parseInt(cmd.getOptionValue("RMI_P", "1100"));
         String serverIp;
         if (cmd.hasOption("debug")) {
-            System.out.println(Utils.ANSI_PURPLE + "Start the Server in DEBUG mode. This sets the timeout to 10 minutes and prints all log messages" + Utils.ANSI_RESET);
+            System.out.println(ANSI_PURPLE + "Start the Server in DEBUG mode. This sets the timeout to 10 minutes and prints all log messages" + Utils.ANSI_RESET);
             HEARTBEAT_TIMEOUT = 600000; //if debug, set the timeout to 10 minutes
+            IS_DEBUG = true;
         }
         if (cmd.hasOption("lan")) {
             try {
@@ -57,7 +67,7 @@ public class Server {
             }
         } else serverIp = cmd.getOptionValue("IP", "localhost");
 
-        System.out.println(Utils.ANSI_PURPLE + "ServerApp IP: " + serverIp + Utils.ANSI_RESET);
+        System.out.println(ANSI_PURPLE + "ServerApp IP: " + serverIp + Utils.ANSI_RESET);
         /* ***************************************
          * START RMI SERVER
          * ***************************************/
@@ -80,11 +90,10 @@ public class Server {
 
         try {
             CommandLine cmd = parser.parse(options, args);
-            if (!cmd.hasOption("TCP_P") || !cmd.hasOption("RMI_P")) {
-                System.err.println("Please specify both ports for TCP and RMI ServerApps.");
-                System.exit(1);
-            }
-            if (Integer.parseInt(cmd.getOptionValue("TCP_P")) == Integer.parseInt(cmd.getOptionValue("RMI_P"))) {
+            TCPPortNumber = cmd.hasOption("TCP_P") ? Integer.parseInt(cmd.getOptionValue("TCP_P")) : 12345; // default is 12345
+            RMIPortNumber = cmd.hasOption("RMI_P") ? Integer.parseInt(cmd.getOptionValue("RMI_P")) : 1099; // default is 1099
+
+            if (cmd.hasOption("TCP_P") && cmd.hasOption("RMI_P") && Integer.parseInt(cmd.getOptionValue("TCP_P")) == Integer.parseInt(cmd.getOptionValue("RMI_P"))) {
                 System.err.println("Please specify different ports for TCP and RMI ServerApps.");
                 System.exit(1);
             }
@@ -107,6 +116,10 @@ public class Server {
             registry.bind("ClientToServerActions", stub);
             System.err.println(Utils.ANSI_YELLOW + "Codex Naturalis RMI Server ready on port: " + RMIPortNumber + Utils.ANSI_RESET);
 
+            if (IS_DEBUG) {
+                inspectRegistry(registry); //Prints all the methods available in the RMI registry
+            }
+
         } catch (RemoteException | AlreadyBoundException e) {
             e.printStackTrace();
         }
@@ -121,6 +134,30 @@ public class Server {
         } catch (IOException e) {
             System.err.println("Could not listen on port " + TCPPortNumber);
             System.exit(-1);
+        }
+    }
+
+    private static void inspectRegistry(Registry registry) throws RemoteException {
+        // Get the list of names bound in the registry
+        String[] boundNames = registry.list();
+
+        System.out.println(ANSI_PURPLE + "Inspecting services available in the RMI registry:" + ANSI_RESET);
+        for (String name : boundNames) {
+            try {
+                Remote remoteObject = registry.lookup(name);
+                Class<?> objClass = remoteObject.getClass();
+
+                System.out.println("Methods available for: " + name);
+                Method[] methods = objClass.getMethods(); // Retrieves all public methods
+
+                for (Method method : methods) {
+                    System.out.println(method.toString());
+                }
+            } catch (Exception e) {
+                System.out.println("Failed to inspect methods for: " + name);
+                e.printStackTrace();
+            }
+            System.out.println();
         }
     }
 }
