6d6b15ae291d3c9a957fa5dc373cf747b0de717d
simonecurci <simonefrancesco.curci@mail.polimi.it>
2024-05-15 17:05:05 +0200

    Added Deck Component

---
+++ src/test/java/it/polimi/ingsw/tui/view/component/drawCards/DecksComponentTest.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.tui.view.component.drawCards;
+
+import it.polimi.ingsw.data.Parser;
+import it.polimi.ingsw.model.card.gameCard.GameCard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DecksComponentTest {
+
+    GameCard firstResourceCard;
+    GameCard firstGoldCard;
+
+    @BeforeEach
+    void setUp() {
+        Parser parser = new Parser();
+        firstResourceCard = parser.getResourceDeck().getCards().getFirst();
+        firstGoldCard = parser.getGoldDeck().getCards().getFirst();
+    }
+
+    @Test
+    @DisplayName("Test if every component of the decks is drawn correctly")
+    void getDecksDrawArea() {
+        System.out.println(new DecksComponent(firstResourceCard, firstGoldCard, 5));
+    }
+}
