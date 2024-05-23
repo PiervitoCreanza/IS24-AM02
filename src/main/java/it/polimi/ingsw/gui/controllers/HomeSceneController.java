d01be1abaf812af18de410a6af63a80879f73351
Pier107 <p.creanza@protonmail.com>
2024-05-23 11:31:51 +0200

    Small improvements

--- src/main/java/it/polimi/ingsw/gui/controllers/HomeSceneController.java
+++ src/main/java/it/polimi/ingsw/gui/controllers/HomeSceneController.java
@@ -1,59 +1,43 @@
 package it.polimi.ingsw.gui.controllers;
 
 import it.polimi.ingsw.network.client.ClientNetworkControllerMapper;
-import it.polimi.ingsw.network.server.message.successMessage.GameRecord;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ProgressIndicator;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
-import java.util.ArrayList;
-
-import static it.polimi.ingsw.gui.GUIApp.DEFAULT_HEIGTH;
-import static it.polimi.ingsw.gui.GUIApp.DEFAULT_WIDTH;
 
 
 public class HomeSceneController extends Controller implements PropertyChangeListener {
 
+    /**
+     * The name of the controller. This is a static variable, meaning it's shared among all instances of this class.
+     */
     public static final ControllersEnum NAME = ControllersEnum.HOME;
+
+    /**
+     * The logger.
+     */
     private final static Logger logger = LogManager.getLogger(HomeSceneController.class);
     @FXML
     private StackPane rootPane;
     @FXML
     private ImageView backgroundImage;
     @FXML
-    private ImageView foregroundImage;
-    @FXML
-    private Button createGameButton;
-    @FXML
-    private Button joinGameButton;
-    @FXML
     private Button quitButton;
     private ClientNetworkControllerMapper networkControllerMapper;
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-    private ProgressIndicator progressIndicator;
-    private ArrayList<GameRecord> gamesList;
 
-    @FXML
-    private ListView<String> gameListView;
-
+    /**
+     * The initialize method is called when the scene and controller are created.
+     */
     @FXML
     public void initialize() {
         logger.debug("Initializing HomeSceneController");
@@ -97,49 +81,8 @@ public class HomeSceneController extends Controller implements PropertyChangeLis
         });
     }
 
-
-    // Method to populate the game list
-    //TODO
-    /*
-    public void populateGameList(ArrayList<GameRecord> games) {
-        gameListView.getItems().clear();
-        gameListView.getItems().addAll(games);
-    }
-*/
-
-    public void refreshList() {
-        //refresh list
-        logger.debug("List refreshed");
-    }
-
-    public void enterGame() {
-        logger.debug("Game entered");
-    }
-
-    public void showLoadingScreen(StackPane rootPane) {
-        this.progressIndicator = new ProgressIndicator();
-        // Add the ProgressIndicator to the root StackPane
-        rootPane.getChildren().add(progressIndicator);
-    }
-
-    public void hideLoadingScreen(StackPane rootPane) {
-        // Remove the ProgressIndicator from the root StackPane
-        rootPane.getChildren().remove(this.progressIndicator);
-    }
-
     public void switchToGamesView(ActionEvent event) throws IOException {
         switchScene(ControllersEnum.GAMES_LIST);
-
-
-    }
-
-    public void switchToHomeView(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("fxml1.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root, DEFAULT_WIDTH, DEFAULT_HEIGTH);
-        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
-        stage.setScene(scene);
-        stage.show();
     }
 
     public void displayDialog(String title, String content) {
