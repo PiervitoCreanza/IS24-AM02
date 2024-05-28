8551c9a7bbd39b6d3d0b3ba1abe1caa046d661e1
simonecurci <simonefrancesco.curci@mail.polimi.it>
2024-05-28 18:55:59 +0200

    Moved connection inside GUIApp to handle RMI connections

--- src/main/java/it/polimi/ingsw/tui/View.java
+++ src/main/java/it/polimi/ingsw/tui/View.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.tui;
 
-import java.beans.PropertyChangeListener;
 
 /**
  * The ViewController interface represents the view controller in the MVC pattern.
  * It extends the PropertyChangeListener interface to listen for changes in the model.
  */
-public interface ViewController extends PropertyChangeListener {
+public interface View {
+    void launchUI();
 }
