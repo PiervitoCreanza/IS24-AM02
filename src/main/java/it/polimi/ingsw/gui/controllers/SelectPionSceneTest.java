4499c43c570bf26f7547cd8a0dc4563fc1947fdd
mattiacolombomc <info@mattiacolombo.com>
2024-05-31 01:11:48 +0200

    Added SelectPionScene with test

---
+++ src/main/java/it/polimi/ingsw/gui/controllers/SelectPionSceneTest.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.gui.controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SelectPionSceneTest extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/selectPionScene.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Select Pion Scene");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMinHeight(900);
+        primaryStage.setMinWidth(1200);
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/gui/controllers/SelectPionTest.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.gui.controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SelectPionTest extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Carica il file FXML per PointsPanel
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/SelectPionScene.fxml"));
+        Parent root = loader.load();
+
+        // Crea una scena con il PointsPanel
+        Scene scene = new Scene(root, 400, 400);
+
+        // Carica il CSS
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+
+        // Configura e mostra lo stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Select Pion Test");
+        primaryStage.show();
+    }
+}
