2e8cfc0686fe2b6065d8a36e77a76d573395be65
Pier107 <p.creanza@protonmail.com>
2024-05-31 23:15:06 +0200

    Edit chat to integrate it in the game scene

--- src/main/java/it/polimi/ingsw/gui/controllers/ChatSidebarController.java
+++ src/main/java/it/polimi/ingsw/gui/controllers/ChatSidebarController.java
@@ -3,31 +3,41 @@ package it.polimi.ingsw.gui.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
 
 public class ChatSidebarController {
 
     @FXML
     private TextArea chatDisplay;
     @FXML
-    private TextField messageInput;
+    private TextArea messageInput;
     @FXML
-    private ComboBox<String> messageType;
-    @FXML
-    private ComboBox<String> playerList;
+    private ComboBox<String> recepient;
 
     @FXML
     private void initialize() {
-        messageType.setOnAction(e -> {
-            playerList.setVisible(messageType.getValue().equals("Private"));
+        recepient.setValue("To Everyone");
+        messageInput.onKeyPressedProperty().set(event -> {
+            if (event.getCode().toString().equals("ENTER")) {
+                handleSend();
+            }
         });
+    }
+
+    public void setUsers(ArrayList<String> users) {
+        recepient.getItems().clear();
+        recepient.getItems().add("To Everyone");
+        for (String user : users) {
+            recepient.getItems().add("To " + user);
+        }
     }
 
     @FXML
     private void handleSend() {
         String message = messageInput.getText();
         if (!message.isEmpty()) {
-            String recipient = messageType.getValue().equals("Private") ? playerList.getValue() : "All";
+            String recipient = recepient.getValue().substring(3);
             chatDisplay.appendText(String.format("[%s]: %s\n", recipient, message));
             messageInput.clear();
         }
