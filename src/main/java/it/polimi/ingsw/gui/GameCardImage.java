1687796385e092c8d674c8264b036ffe59ab7521
Pier107 <p.creanza@protonmail.com>
2024-05-31 23:16:38 +0200

    Add mutli-side support

--- src/main/java/it/polimi/ingsw/gui/GameCardImage.java
+++ src/main/java/it/polimi/ingsw/gui/GameCardImage.java
@@ -12,7 +12,10 @@ import java.net.URL;
 public class GameCardImage {
     private final URL front;
     private final URL back;
+    private static final double cardWidth = 234;
     private final int cardId;
+    private static final double cardRatio = 1.5;
+    private boolean isFront = true;
 
     /**
      * This method loads the images for the card from resources.
@@ -26,6 +29,26 @@ public class GameCardImage {
         this.cardId = cardId;
         String paddedCardId = String.format("%03d", cardId);
         this.front = getClass().getResource("/" + paddedCardId + "_front.png");
+
+        // Determine the file name for the back image based on the cardId.
+        if (cardId < 11) {
+            paddedCardId = "001";
+        } else if (cardId < 21) {
+            paddedCardId = "011";
+        } else if (cardId < 31) {
+            paddedCardId = "021";
+        } else if (cardId < 41) {
+            paddedCardId = "031";
+        } else if (cardId < 51) {
+            paddedCardId = "041";
+        } else if (cardId < 66) {
+            paddedCardId = "051";
+        } else if (cardId < 73) {
+            paddedCardId = "066";
+        } else if (88 >= cardId) {
+            paddedCardId = "102";
+        }
+
         this.back = getClass().getResource("/" + paddedCardId + "_back.png");
 
         if (this.front == null) {
@@ -33,13 +56,14 @@ public class GameCardImage {
         }
     }
 
-    private ImageView createImageView(URL imageUrl) {
-        Image image = new Image(imageUrl.toString(), true);
-        ImageView imageView = new ImageView(image);
-        imageView.preserveRatioProperty().set(true);
-        imageView.fitWidthProperty().set(234);
-        imageView.getStyleClass().add("card");
-        return imageView;
+    /**
+     * This method returns the width of the card image.
+     *
+     * @param width The width of the card image.
+     * @return The width of the card image.
+     */
+    public static double getHeightFromWidth(double width) {
+        return width / cardRatio;
     }
 
     /**
@@ -62,6 +86,30 @@ public class GameCardImage {
         return createImageView(this.back);
     }
 
+    private ImageView createImageView(URL imageUrl) {
+        Image image = new Image(imageUrl.toString(), true);
+        ImageView imageView = new ImageView(image);
+        imageView.preserveRatioProperty().set(true);
+        imageView.setFitWidth(cardWidth);
+        imageView.getStyleClass().add("card");
+        return imageView;
+    }
+
+    /**
+     * This method returns an ImageView of the front or back of the card, depending on the current side.
+     * The image is loaded from the URL stored in the front or back field.
+     *
+     * @return An ImageView of the front or back of the card.
+     */
+    public ImageView switchSide() {
+        if (isFront) {
+            isFront = false;
+            return createImageView(this.back);
+        }
+        isFront = true;
+        return createImageView(this.front);
+    }
+
     /**
      * This method returns the cardId associated with the image.
      *
@@ -69,5 +117,18 @@ public class GameCardImage {
      */
     public int getCardId() {
         return cardId;
+    }
+
+    /**
+     * This method returns an ImageView of the current side of the card.
+     * The image is loaded from the URL stored in the front or back field, depending on the current side.
+     *
+     * @return An ImageView of the current side of the card.
+     */
+    public ImageView getCurrentSide() {
+        if (isFront) {
+            return createImageView(this.front);
+        }
+        return createImageView(this.back);
     }
 }
