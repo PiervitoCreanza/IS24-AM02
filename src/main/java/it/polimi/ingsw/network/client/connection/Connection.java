3fcc83a349f530b95d8800622ae785dd63c156d7
simonecurci <simonefrancesco.curci@mail.polimi.it>
2024-06-14 11:15:30 +0200

    Unified connection

--- src/main/java/it/polimi/ingsw/network/client/connection/Connection.java
+++ src/main/java/it/polimi/ingsw/network/client/connection/Connection.java
@@ -1,20 +1,89 @@
 package it.polimi.ingsw.network.client.connection;
 
+import it.polimi.ingsw.network.client.ClientNetworkControllerMapper;
 import it.polimi.ingsw.utils.PropertyChangeNotifier;
+import org.apache.logging.log4j.Logger;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 /**
  * The Connection interface represents a network connection in the application.
  * It extends the PropertyChangeNotifier interface to provide property change support.
  */
-public interface Connection extends PropertyChangeNotifier {
+public abstract class Connection implements PropertyChangeNotifier, PropertyChangeListener {
+
+    protected int attempts = 1;
+
+    protected int maxAttempts = 5;
+
+    protected long waitTime = 5000;
+
+    protected ClientNetworkControllerMapper networkControllerMapper;
+
+    protected String serverIp;
+
+    protected int serverPort;
+
+    protected PropertyChangeSupport listeners;
+
+    protected Logger logger;
+
+    public Connection(ClientNetworkControllerMapper networkControllerMapper, String serverIp, int serverPort) {
+        this.networkControllerMapper = networkControllerMapper;
+        this.serverIp = serverIp;
+        this.serverPort = serverPort;
+        this.listeners = new PropertyChangeSupport(this);
+    }
 
     /**
      * Establishes a connection.
      */
-    void connect();
+    public abstract void connect();
 
     /**
      * Closes the program when it's impossible to establish a connection.
      */
-    void quit();
+    protected void quit() {
+        System.exit(-1);
+    }
+
+    /**
+     * This method is called when a property change event is fired.
+     * It is responsible for handling the property change event.
+     *
+     * @param evt The property change event that was fired
+     */
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        String changedProperty = evt.getPropertyName();
+        if (changedProperty.equals("CONNECTION_CLOSED")) {
+            connect();
+        } else {
+            logger.warn("Unknown property change event: {}", changedProperty);
+        }
+    }
+
+    /**
+     * Adds a PropertyChangeListener to the listeners list.
+     * The listener will be notified of property changes.
+     *
+     * @param listener The PropertyChangeListener to be added
+     */
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.listeners.addPropertyChangeListener(listener);
+    }
+
+    /**
+     * Removes a PropertyChangeListener from the listeners list.
+     * The listener will no longer be notified of property changes.
+     *
+     * @param listener The PropertyChangeListener to be removed
+     */
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        this.listeners.removePropertyChangeListener(listener);
+    }
 }
