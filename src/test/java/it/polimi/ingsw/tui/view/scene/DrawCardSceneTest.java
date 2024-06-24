ade04505611d6fc26544d7f981f32065d612fdf1
Pier107 <p.creanza@protonmail.com>
2024-06-24 14:48:15 +0200

    Fix test

--- src/test/java/it/polimi/ingsw/tui/view/scene/DrawCardSceneTest.java
+++ src/test/java/it/polimi/ingsw/tui/view/scene/DrawCardSceneTest.java
@@ -51,7 +51,7 @@ public class DrawCardSceneTest {
         ArrayList<GameCard> hand = player1.getVirtualView().playerHandView().hand();
         GlobalBoardView globalBoardView = game.getVirtualView().globalBoardView();
 
-        DrawCardScene drawCardScene = new DrawCardScene(null, playerBoardView, globalObjectives, playerObjective, hand, globalBoardView, true, 0);
+        DrawCardScene drawCardScene = new DrawCardScene(null, playerBoardView, globalObjectives, playerObjective, hand, globalBoardView, true, 0, 0);
         drawCardScene.getDrawArea().println();
     }
 }
