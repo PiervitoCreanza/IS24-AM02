dbf2f94216d6e65860a778267fdf05e30702af4d
mattiacolombomc <info@mattiacolombo.com>
2024-06-27 10:40:59 +0200

    Try to fix full screen

--- src/main/java/it/polimi/ingsw/view/gui/GUIApp.java
+++ src/main/java/it/polimi/ingsw/view/gui/GUIApp.java
@@ -1,25 +1,25 @@
-package it.polimi.ingsw.view.gui;
+package it.polimi.ingsw.view.gui;
 
-import it.polimi.ingsw.view.gui.components.toast.ToastLevels;
-import it.polimi.ingsw.view.gui.controllers.Controller;
-import it.polimi.ingsw.view.tui.View;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import it.polimi.ingsw.view.gui.components.toast.ToastLevels;
+import it.polimi.ingsw.view.gui.controllers.Controller;
+import it.polimi.ingsw.view.tui.View;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.awt.*;
-import java.io.IOException;
-import java.net.URL;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Class representing the GUI application.
@@ -29,17 +29,17 @@ public class GUIApp extends Application implements View {
     /**
      * Logger for this class.
      */
-    private static final Logger logger = LogManager.getLogger(GUIApp.class);
+    private static final Logger logger = LogManager.getLogger(GUIApp.class);
 
     /**
      * Minimum width of the application window.
      */
-    public static int MIN_WIDTH = 1560;
+    public static int MIN_WIDTH = 1560;
 
     /**
      * Minimum height of the application window.
      */
-    public static int MIN_HEIGHT = 900;
+    public static int MIN_HEIGHT = 900;
 
     /**
      * Method to launch the UI.
@@ -49,12 +49,12 @@ public class GUIApp extends Application implements View {
      */
     @Override
     public void launchUI() {
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        logger.debug("Screen size is {}x{}", screenSize.width, screenSize.height);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        logger.debug("Screen size is {}x{}", screenSize.width, screenSize.height);
         if (screenSize.width < MIN_WIDTH || screenSize.height < MIN_HEIGHT) {
-            System.setProperty("prism.allowhidpi", "false");
+            System.setProperty("prism.allowhidpi", "false");
         }
-        launch();
+        launch();
     }
 
     /**
@@ -63,8 +63,8 @@ public class GUIApp extends Application implements View {
      */
     @Override
     public void init() {
-        logger.info("GUIApp started");
-        loadFonts();
+        logger.info("GUIApp started");
+        loadFonts();
     }
 
     /**
@@ -79,68 +79,68 @@ public class GUIApp extends Application implements View {
 
         // Set the stage to close the application when the window is closed.
         stage.setOnCloseRequest(event -> {
-            logger.info("GUIApp quit");
-            Platform.exit();
-            System.exit(0);
-        });
+            logger.info("GUIApp quit");
+            Platform.exit();
+            System.exit(0);
+        });
 
         // Set the stage title and dimensions.
-        stage.setTitle("Codex Naturalis");
-        stage.setMinHeight(MIN_HEIGHT);
-        stage.setMinWidth(MIN_WIDTH);
-        stage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("icon_512x512.png")));
+        stage.setTitle("Codex Naturalis");
+        stage.setMinHeight(MIN_HEIGHT);
+        stage.setMinWidth(MIN_WIDTH);
+        stage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("icon_512x512.png")));
 
         // Set the current stage on all controllers.
-        Controller.setStage(stage);
+        Controller.setStage(stage);
 
-        URL fxml1URL = getClass().getResource("/HomeScene.fxml");
+        URL fxml1URL = getClass().getResource("/HomeScene.fxml");
         if (fxml1URL == null) {
-            throw new IOException("Cannot load resource: HomeScene.fxml");
+            throw new IOException("Cannot load resource: HomeScene.fxml");
         }
-        FXMLLoader loader = new FXMLLoader(fxml1URL);
+        FXMLLoader loader = new FXMLLoader(fxml1URL);
 
         // Load the scene
-        Parent root = loader.load();
+        Parent root = loader.load();
 
-        Scene scene = new Scene(root, MIN_WIDTH, MIN_HEIGHT);
+        Scene scene = new Scene(root, MIN_WIDTH, MIN_HEIGHT);
 
-        String fullScreenButton;
+        String fullScreenButton;
         // Set the full screen button based on the OS.
         if (!System.getProperty("os.name").toLowerCase().contains("mac")) {
-            fullScreenButton = "F11";
+            fullScreenButton = "F11";
         } else {
-            fullScreenButton = "Cmd+F";
+            fullScreenButton = "Cmd+F";
         }
         // Show a toast message when entering full screen.
         stage.fullScreenProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                Controller.showToast(ToastLevels.INFO, "Full screen", "Press " + fullScreenButton + " to exit full screen");
+                Controller.showToast(ToastLevels.INFO, "Full screen", "Press " + fullScreenButton + " to exit full screen");
             }
-        });
-        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+        });
+        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         // Set full screen shortcut
         scene.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
             if (System.getProperty("os.name").toLowerCase().contains("mac") && event.isMetaDown() && event.getCode() == KeyCode.F) {
-                stage.setFullScreen(!stage.isFullScreen());
+                stage.setFullScreen(!stage.isFullScreen());
             } else if (event.getCode() == KeyCode.F11) {
-                stage.setFullScreen(!stage.isFullScreen());
+                stage.setFullScreen(!stage.isFullScreen());
             }
-        });
+        });
 
         // Set the stage to the current scene.
-        stage.setScene(scene);
+        stage.setScene(scene);
 
         // Set the current scene on all controllers.
-        Controller.setScene(scene);
+        Controller.setScene(scene);
 
         // Get the controller from the loader.
-        Controller controller = loader.getController();
+        Controller controller = loader.getController();
 
         // Call the beforeShow method of the controller.
         // This method is used to perform actions right before the window is shown.
-        controller.beforeMount(null);
+        controller.beforeMount(null);
 
-        stage.show();
+        stage.show();
     }
 
     /**
@@ -148,7 +148,7 @@ public class GUIApp extends Application implements View {
      * It loads the MedievalSharp-Book and CalSans-SemiBold fonts.
      */
     private void loadFonts() {
-        Font.loadFont(getClass().getResourceAsStream("/fonts/MedievalSharp-Book.ttf"), 10);
-        Font.loadFont(getClass().getResourceAsStream("/fonts/CalSans-SemiBold.ttf"), 10);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/MedievalSharp-Book.ttf"), 10);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/CalSans-SemiBold.ttf"), 10);
     }
 }
