9fbc043823fbc3fb0c72b52b8effb8df688c1664
Pier107 <p.creanza@protonmail.com>
2024-06-29 01:23:20 +0200

    Fix bug in docker-compose

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,5 @@ GHCR_AUTH_TOKEN=$2
 #Login to GitHub Container Registry
 echo "$GHCR_AUTH_TOKEN" | docker login ghcr.io -u piervitocreanza --password-stdin
 
-# Pull the image
-docker pull ghcr.io/piervitocreanza/is24-am02:latest
-
 # Run the docker compose
 docker-compose up -d
