dfcce5467e23594a00dc8a5ae977eb43b76cb9fa
mattiacolombomc <info@mattiacolombo.com>
2024-07-02 19:48:31 +0200

    Added slides

--- src/main/java/it/polimi/ingsw/model/utils/PointCornerPositionPair.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.model.utils;
-
-import it.polimi.ingsw.model.card.corner.CornerPosition;
-
-/**
- * This class represents a pair of a point and a corner position.
- */
-public record PointCornerPositionPair(Coordinate coordinate, CornerPosition cornerPosition) {
-}
